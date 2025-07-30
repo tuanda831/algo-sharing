e442c1ca56e8c94a286c00acae3a52c5f687b665
tuando <tuanda831@gmail.com>
2025-07-30 20:48:37 +0700

    feat: correct the solution 61

--- sandbox/java/src/main/java/com/algo/sandbox/Task61.java
+++ sandbox/java/src/main/java/com/algo/sandbox/Task61.java
@@ -8,7 +8,6 @@ import java.util.Map;
 public class Task61 {
     public int solution(String s, int[] c) {
         Map<Character, List<Integer>> charPositions = new HashMap<>();
-
         for (int i = 0; i < s.length(); i++) {
             charPositions.computeIfAbsent(
                     s.charAt(i),
@@ -21,11 +20,11 @@ public class Task61 {
         for (List<Integer> positions : charPositions.values()) {
             if (positions.size() == 1) continue;
 
-            int cIndex = 0;
-
             for (int i = 0; i < positions.size() - 1; i++) {
                 int left = positions.get(i);
                 int right = positions.get(i + 1);
+
+                int cIndex = 0;
                 boolean found = false;
 
                 while (cIndex < c.length) {
@@ -41,9 +40,8 @@ public class Task61 {
                 if (!found) {
                     return -1;
                 }
+                minInsertionNeeded = Math.max(minInsertionNeeded, cIndex);
             }
-
-            minInsertionNeeded = Math.max(minInsertionNeeded, cIndex);
         }
 
         return minInsertionNeeded;
--- sandbox/java/src/test/java/com/algo/sandbox/Task61Test.java
+++ sandbox/java/src/test/java/com/algo/sandbox/Task61Test.java
@@ -2,6 +2,9 @@ package com.algo.sandbox;
 
 import junit.framework.TestCase;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Task61Test extends TestCase {
     private Task61 task = new Task61();
 
@@ -10,5 +13,9 @@ public class Task61Test extends TestCase {
         assertEquals(2, task.solution("aaa", new int[]{1, 2}));
         assertEquals(3, task.solution("aabcddcb", new int[]{3, 5, 1, 4, 7}));
         assertEquals(-1, task.solution("wkwk", new int[]{1}));
+
+        String s = IntStream.range(0, 50000).mapToObj(i -> "a").collect(Collectors.joining());
+        int[] a = IntStream.range(0, 50000).map(i -> 50000 - i - 1).toArray();
+        assertEquals(49999, task.solution(s, a));
     }
 }
